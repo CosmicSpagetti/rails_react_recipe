160bb9e6942939801090f4598ddbda8aa817b045
CosmicSpagetti <billyurrutia@gmail.com>
2023-07-27 10:59:06 -0600

    add additional comments on whats being done

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Routes from "../routes";
 
+// export component to render the routes within fragments. This component will render at applications entry
+// point making the routes available whenever the application is loaded
 export default props => <>{Routes}</>;
 
--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+// create root element as div appended to page and render app component in it
 document.addEventlistener("turbo:load", () => {
   const root = createRoot(
     document.body.appendChild(document.createElement("div"))
